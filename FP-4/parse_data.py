f0bc0488a9f797318fdfa6d897e937a32cd4d080
K-hayasi <wtree0333@gmail.com>
2026-01-16 01:19:22 +0900

    parse all corrected

--- FP-4/parse_data.py
+++ FP-4/parse_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
-df0 = pd.read_csv('winequality-red.csv')
+df0 = pd.read_csv('Real estate valuation data set.csv')
 df0.describe()
 
-df = df0[  ['quality', 'volatile acidity', 'density', 'residual sugar']  ]
-df = df.rename( columns={'volatile acidity':'acidity', 'residual sugar':'sugar'} )
+df = df0[  ['Y house price of unit area', 'X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores']  ]
+df = df.rename( columns={'Y house price of unit area':'price','X2 house age':'age', 'X3 distance to the nearest MRT station':'from station', 'X4 number of convenience stores':'number of convenience stores'} )
 df.describe()
